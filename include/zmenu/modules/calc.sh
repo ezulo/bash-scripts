c0fa2a18ac666384a7666920d35db0536f07f33e
ezuloaga0@gmail.com <ezuloaga0@gmail.com>
2025-07-31 16:13:33 -0400

    zmenu: Add a calculator

---
+++ include/zmenu/modules/calc.sh
@@ -0,0 +1,18 @@
+#!/bin/echo Please-Source
+
+# Backend can be changed, though usage below must be adapted
+BACKEND=/usr/bin/bc
+! command -v "$BACKEND" > /dev/null &&
+    log_error "$ID" "$BACKEND not found." && exit 1
+
+PROMPT="Enter a calculation"
+CALC=$(d_read_cached "$ID" "history" "$PROMPT" "no_write")
+
+RES=$(echo "$CALC" | "$BACKEND" 2>&1)
+
+[[ "$RES" == *"error"* ]] &&
+    log_error "$ID" "Error: could not calculate." && exit 1
+
+d_cache_append "$ID" "history" "$CALC"
+log_info "$ID" "$CALC =\n$RES"
+
