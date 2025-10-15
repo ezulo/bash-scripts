a3bb5b006304c52bde8326ba8866d074e9caf906
ezulo <ezuloaga0@gmail.com>
2025-10-14 22:46:25 -0400

    themectl: harden hyprpaper module; now works with toggle_wallpaper

--- include/themectl/modules/hyprpaper.sh
+++ include/themectl/modules/hyprpaper.sh
@@ -1,38 +1,50 @@
 #!/bin/echo Please-Source
 
 ID_HYPRPAPER="$_ID:hyprpaper"
+HYPR_CONFIG_DIR="${HYPR_CONFIG_DIR:-$XDG_CONFIG_HOME/hypr}"
+WALLPAPER_DIR="$THEME_HOME/$THEME/wallpapers"
+WALLPAPERS=$(find "$WALLPAPER_DIR" -type f 2> /dev/null | shuf)
+
+_is_active() {
+    ! command -v hyprpaper > /dev/null && return 1
+    [ -z "$WALLPAPERS" ] || [ ! -d "$WALLPAPER_DIR" ] &&
+        pgrep -f hyprpaper > /dev/null && killall hyprpaper > /dev/null && return 1
+    return 0
+}
+
+_monitors() {
+	echo $(hyprctl monitors -j | jq -r -r '.[] | .name')
+}
 
 hyprpaper_theme() {
     local ID="$ID_HYPRPAPER:theme"
-    log_trace $ID "Nothing done."
-}
-
-hyprpaper_reload() {
-    local ID="$ID_HYPRPAPER:reload"
+    ! _is_active && return 0
     # Ignore if command does not exist
-    ! command -v hyprpaper > /dev/null && 
-        log_warn $ID "hyprpaper command not found." && return 1
-    WALLPAPER_DIR="$THEME_HOME/$THEME/wallpapers"
-    WALLPAPERS=$(find "$WALLPAPER_DIR" -type f | shuf)
-    if [ ! -d "$WALLPAPER_DIR" ] || [ -z "$WALLPAPERS" ]; then
-        pgrep -f hyprpaper > /dev/null && killall hyprpaper
-        return 0
-    fi
-    if ! pgrep -f hyprpaper > /dev/null; then
-        hyprpaper > /dev/null 2>&1 &
-    fi
-	MONITORS=$(hyprctl monitors -j | jq -r -r '.[] | .name')
+    MONITORS=($(_monitors))
 	echo -n "" > "$HYPR_CONFIG_DIR/hyprpaper.conf"
     i=1
-	for MONITOR in $MONITORS; do
+	for MONITOR in "${MONITORS[@]}"; do
 		WALLPAPER=$(echo "$WALLPAPERS"| head -n $i | tail -n 1)
 		echo "preload = $WALLPAPER" >> "$HYPR_CONFIG_DIR/hyprpaper.conf"
 		echo "wallpaper = $MONITOR, $WALLPAPER" >> \
             "$HYPR_CONFIG_DIR/hyprpaper.conf"
-		hyprctl hyprpaper reload "$MONITOR","$WALLPAPER" &> /dev/null 2>&1
-        sleep 0.2 # Sometimes hyprpaper has a hard time keeping up.
         i=$(($i+1))
 	done
+}
+
+hyprpaper_reload() {
+    local ID="$ID_HYPRPAPER:reload"
+    ! _is_active && return 0
+    # Kill hyprpaper if there are no wallpapers
+    # Start hyprpaper if it isn't running
+    ! pgrep -f hyprpaper > /dev/null && hyprpaper > /dev/null 2>&1 &
+    MONITORS=($(_monitors))
+    i=1
+    for MONITOR in "${MONITORS[@]}"; do
+		WALLPAPER=$(echo "$WALLPAPERS"| head -n $i | tail -n 1)
+        hyprctl hyprpaper reload "$MONITOR","$WALLPAPER" &> /dev/null 2>&1
+        i=$(($i+1))
+    done
 }
 
 hyprpaper_clean() {
