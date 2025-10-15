e827a74685447b9c362fb6f2a2a7d5a56fc911a8
ezulo <ezuloaga0@gmail.com>
2025-10-14 22:44:16 -0400

    themectl and zmenu: add ability to toggle wallpapers

--- include/zmenu/modules/theme.sh
+++ include/zmenu/modules/theme.sh
@@ -5,7 +5,7 @@ ID="$_ID:theme"
 # Const
 LFS='
 '
-OPTS="get\nset\nreload\nclear\ncreate\n"
+OPTS="get\nset\nreload\nclear\ncreate\ntoggle wallpapers"
 
 THEMECTL="$XDG_CONFIG_HOME/scripts/themectl"
 THEME_HOME="$XDG_CONFIG_HOME/theme"
@@ -25,7 +25,7 @@ case $OPT in
         ;;
     set)
         ID="$ID:set"
-        THEME_OPT=$(d_read "$ID" "$AV_THEMES" "Select a theme to set.")
+        THEME_OPT=$(d_read "$ID" "$AV_THEMES" "set theme")
         [ -z $(echo "$AV_THEMES" | grep "^$THEME_OPT\$") ] &&
             log_error "$ID" "Theme not found: $THEME_OPT" && exit 1
         $("$THEMECTL" set "$THEME_OPT")
@@ -41,13 +41,21 @@ case $OPT in
     create)
         ID="$ID:create"
         SRC_THEME=
-        d_read_yes_no "$ID" "Duplicate an existing theme?" &&
-            SRC_THEME=$(d_read "$ID" "$AV_THEMES" "Select a source theme") &&
+        d_read_yes_no "$ID" "duplicate existing?" &&
+            SRC_THEME=$(d_read "$ID" "$AV_THEMES" "source theme") &&
             [ -z $(echo "$AV_THEMES" | grep "^$SRC_THEME\$") ] &&
                 log_error "$ID" "Theme not found: $THEME_OPT" && exit 1
         NEW_THEME=$(k_read "$ID" "What would you like to name your new theme?")
-        [ -z "$NEW_THEME" ] && log_error "$ID" "No theme selected." && exit 1
+        [ -z "$NEW_THEME" ] && log_error "$ID" "Theme name was empty." && exit 1
         "$THEMECTL" create "$NEW_THEME" "$SRC_THEME" &&
         "$TERMINAL" "$EDITOR" "$THEME_HOME/$NEW_THEME"
         ;;
+    'toggle wallpapers')
+        ID="$ID:toggle_wallpapers"
+        "$THEMECTL" toggle-wallpapers
+        ;;
+    *)
+        log_error "$ID" "Unrecognized option: $OPT" && exit 1
+        ;;
 esac
+
